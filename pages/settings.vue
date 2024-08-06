550f0aa705a5dc23fa00c80ed407a7c4ec7b837b
Rdkang <68774237+Rdkang@users.noreply.github.com>
2024-08-06 23:03:48 +0100

    chore: add more dateFormat options

--- pages/settings.vue
+++ pages/settings.vue
@@ -346,8 +346,28 @@ export default {
           value: 'dd/MM/yyyy'
         },
         {
-          text: 'YYYY/MM/DD',
-          value: 'yyyy/MM/dd'
+          text: 'DD.MM.YYYY',
+          value: 'dd.MM.yyyy'
+        },
+        {
+          text: 'YYYY-MM-DD',
+          value: 'yyyy-MM-dd'
+        },
+        {
+          text: 'MMM do, yyyy',
+          value: 'MMM do, yyyy'
+        },
+        {
+          text: 'MMMM do, yyyy',
+          value: 'MMMM do, yyyy'
+        },
+        {
+          text: 'dd MMM yyyy',
+          value: 'dd MMM yyyy'
+        },
+        {
+          text: 'dd MMMM yyyy',
+          value: 'dd MMMM yyyy'
         }
       ]
     },
